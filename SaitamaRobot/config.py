683a96761d743e22928a214f6620c048cad4a12a
Nikkin5666 <86043783+Nikkin5666@users.noreply.github.com>
2021-06-27 16:02:14 +0530

    Add files via upload

---
+++ SaitamaRobot/config.py
@@ -0,0 +1,12 @@
+ from SaitamaRobot.sample_config import Config
+
+class Development(Config):
+    OWNER_ID = 1865747807
+    OWNER_USERNAME = Non_Bonkable_Being
+    API_KEY = 1762083152:AAH1g1NHvkCUE9W9a7bdG8vIlNBTbqxuXoo
+    SQLALCHEMY_DATABASE_URI = postgresql://u0_a320:md5415ac58c648408a7a444f3716fa1f9e1@localhost:5432/saithama
+    JOIN_LOGGER = -1001410415657
+    USE_JOIN_LOGGER = True
+    DRAGONS = []
+    LOAD = []
+    NO_LOAD = ['translation']
